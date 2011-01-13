8f88c6ac0e62279967f48711f47fe65e984f4451
Dane Bertram <dbertram@users.noreply.github.com>
2011-01-12 22:51:49 -0500

    add support for opening and saving vacation data files...yay!

---
+++ Fogcation/VacationData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Fogcation
+{
+    [Serializable]
+    public class VacationData
+    {
+        [XmlIgnore]
+        public string sFileName = null;
+        [XmlIgnore]
+        public bool fLoading = false;
+        [XmlIgnore]
+        public bool fDirty = false;
+
+        [XmlIgnore]
+        public Version FileFormat = frmMain.fileFormat;
+
+        [XmlAttribute("FileFormat")]
+        public string XmlFileFormat
+        {
+            get { return FileFormat.ToString(); }
+            set { FileFormat = new Version(value); }
+        }
+
+        public DateTime StartDate;
+
+        [XmlIgnore]
+        public TimeSpan StartBalance;
+
+        [XmlElement("StartBalance", DataType = "duration")]
+        [DefaultValue("PT0M")]
+        public string XmlStartBalance
+        {
+            get { return XmlConvert.ToString(StartBalance); }
+            set { StartBalance = XmlConvert.ToTimeSpan(value); }
+        }
+
+        public List<VacationDay> VacationDays = new List<VacationDay>();
+        public DateTime TargetDate;
+    }
+}
--- Fogcation/frmMain.Designer.cs
+++ Fogcation/frmMain.Designer.cs
@@ -42,6 +42,7 @@
             this.lblCurrBalance = new System.Windows.Forms.Label();
             this.lstLog = new System.Windows.Forms.ListView();
             this.colType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colDelta = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.colBalanceHours = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.colBalanceDays = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.lblVacation = new System.Windows.Forms.Label();
@@ -52,23 +53,33 @@
             this.btnAdd = new System.Windows.Forms.Button();
             this.btnRemove = new System.Windows.Forms.Button();
             this.btnClear = new System.Windows.Forms.Button();
-            this.colDelta = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.dlgSave = new System.Windows.Forms.SaveFileDialog();
+            this.mnuMain = new System.Windows.Forms.MenuStrip();
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFileNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFileOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFileSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFileSaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuFileExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.dlgOpen = new System.Windows.Forms.OpenFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.mnuMain.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblCurr
             // 
             this.lblCurr.AutoSize = true;
-            this.lblCurr.Location = new System.Drawing.Point(12, 9);
+            this.lblCurr.Location = new System.Drawing.Point(12, 31);
             this.lblCurr.Name = "lblCurr";
-            this.lblCurr.Size = new System.Drawing.Size(127, 17);
+            this.lblCurr.Size = new System.Drawing.Size(133, 17);
             this.lblCurr.TabIndex = 0;
-            this.lblCurr.Text = "Current balance as of";
+            this.lblCurr.Text = "Vacation balance as of";
             // 
             // dtCurr
             // 
             this.dtCurr.CustomFormat = "";
-            this.dtCurr.Location = new System.Drawing.Point(151, 6);
+            this.dtCurr.Location = new System.Drawing.Point(151, 28);
             this.dtCurr.Name = "dtCurr";
             this.dtCurr.Size = new System.Drawing.Size(200, 24);
             this.dtCurr.TabIndex = 1;
@@ -76,7 +87,7 @@
             // 
             // txtCurrBalance
             // 
-            this.txtCurrBalance.Location = new System.Drawing.Point(12, 33);
+            this.txtCurrBalance.Location = new System.Drawing.Point(12, 55);
             this.txtCurrBalance.Name = "txtCurrBalance";
             this.txtCurrBalance.Size = new System.Drawing.Size(70, 24);
             this.txtCurrBalance.TabIndex = 3;
@@ -91,7 +102,7 @@
             // lblFuture
             // 
             this.lblFuture.AutoSize = true;
-            this.lblFuture.Location = new System.Drawing.Point(12, 255);
+            this.lblFuture.Location = new System.Drawing.Point(12, 277);
             this.lblFuture.Name = "lblFuture";
             this.lblFuture.Size = new System.Drawing.Size(136, 17);
             this.lblFuture.TabIndex = 4;
@@ -100,7 +111,7 @@
             // dtFuture
             // 
             this.dtFuture.CustomFormat = "";
-            this.dtFuture.Location = new System.Drawing.Point(151, 252);
+            this.dtFuture.Location = new System.Drawing.Point(151, 274);
             this.dtFuture.Name = "dtFuture";
             this.dtFuture.Size = new System.Drawing.Size(200, 24);
             this.dtFuture.TabIndex = 5;
@@ -109,7 +120,7 @@
             // lblCurrPayPeriod
             // 
             this.lblCurrPayPeriod.AutoSize = true;
-            this.lblCurrPayPeriod.Location = new System.Drawing.Point(365, 9);
+            this.lblCurrPayPeriod.Location = new System.Drawing.Point(365, 31);
             this.lblCurrPayPeriod.Name = "lblCurrPayPeriod";
             this.lblCurrPayPeriod.Size = new System.Drawing.Size(67, 17);
             this.lblCurrPayPeriod.TabIndex = 2;
@@ -118,7 +129,7 @@
             // lblFuturePayPeriod
             // 
             this.lblFuturePayPeriod.AutoSize = true;
-            this.lblFuturePayPeriod.Location = new System.Drawing.Point(365, 255);
+            this.lblFuturePayPeriod.Location = new System.Drawing.Point(365, 277);
             this.lblFuturePayPeriod.Name = "lblFuturePayPeriod";
             this.lblFuturePayPeriod.Size = new System.Drawing.Size(67, 17);
             this.lblFuturePayPeriod.TabIndex = 6;
@@ -127,7 +138,7 @@
             // lblCurrBalance
             // 
             this.lblCurrBalance.AutoSize = true;
-            this.lblCurrBalance.Location = new System.Drawing.Point(96, 36);
+            this.lblCurrBalance.Location = new System.Drawing.Point(96, 58);
             this.lblCurrBalance.Name = "lblCurrBalance";
             this.lblCurrBalance.Size = new System.Drawing.Size(60, 17);
             this.lblCurrBalance.TabIndex = 8;
@@ -147,9 +158,9 @@
             this.lstLog.GridLines = true;
             this.lstLog.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
             this.lstLog.HideSelection = false;
-            this.lstLog.Location = new System.Drawing.Point(15, 290);
+            this.lstLog.Location = new System.Drawing.Point(15, 312);
             this.lstLog.Name = "lstLog";
-            this.lstLog.Size = new System.Drawing.Size(647, 124);
+            this.lstLog.Size = new System.Drawing.Size(647, 126);
             this.lstLog.TabIndex = 15;
             this.lstLog.UseCompatibleStateImageBehavior = false;
             this.lstLog.View = System.Windows.Forms.View.Details;
@@ -158,6 +169,12 @@
             // 
             this.colType.Text = "";
             this.colType.Width = 254;
+            // 
+            // colDelta
+            // 
+            this.colDelta.Text = "Change in hours";
+            this.colDelta.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.colDelta.Width = 116;
             // 
             // colBalanceHours
             // 
@@ -174,7 +191,7 @@
             // lblVacation
             // 
             this.lblVacation.AutoSize = true;
-            this.lblVacation.Location = new System.Drawing.Point(12, 69);
+            this.lblVacation.Location = new System.Drawing.Point(12, 91);
             this.lblVacation.Name = "lblVacation";
             this.lblVacation.Size = new System.Drawing.Size(91, 17);
             this.lblVacation.TabIndex = 16;
@@ -192,7 +209,7 @@
             this.lstVacation.GridLines = true;
             this.lstVacation.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
             this.lstVacation.HideSelection = false;
-            this.lstVacation.Location = new System.Drawing.Point(15, 89);
+            this.lstVacation.Location = new System.Drawing.Point(15, 111);
             this.lstVacation.Name = "lstVacation";
             this.lstVacation.Size = new System.Drawing.Size(556, 145);
             this.lstVacation.TabIndex = 17;
@@ -219,7 +236,7 @@
             // btnAdd
             // 
             this.btnAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnAdd.Location = new System.Drawing.Point(587, 89);
+            this.btnAdd.Location = new System.Drawing.Point(587, 111);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(75, 23);
             this.btnAdd.TabIndex = 18;
@@ -230,7 +247,7 @@
             // btnRemove
             // 
             this.btnRemove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnRemove.Location = new System.Drawing.Point(587, 119);
+            this.btnRemove.Location = new System.Drawing.Point(587, 141);
             this.btnRemove.Name = "btnRemove";
             this.btnRemove.Size = new System.Drawing.Size(75, 23);
             this.btnRemove.TabIndex = 19;
@@ -241,7 +258,7 @@
             // btnClear
             // 
             this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnClear.Location = new System.Drawing.Point(587, 149);
+            this.btnClear.Location = new System.Drawing.Point(587, 171);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(75, 23);
             this.btnClear.TabIndex = 20;
@@ -249,17 +266,91 @@
             this.btnClear.UseVisualStyleBackColor = true;
             this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
-            // colDelta
+            // dlgSave
             // 
-            this.colDelta.Text = "Change in hours";
-            this.colDelta.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.colDelta.Width = 116;
+            this.dlgSave.DefaultExt = "xml";
+            this.dlgSave.FileName = "fog_creek_vacation";
+            this.dlgSave.Filter = "XML Files (*.xml)|*.xml|All files (*.*)|*.*";
+            this.dlgSave.Title = "Save vacation data...";
+            // 
+            // mnuMain
+            // 
+            this.mnuMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFile});
+            this.mnuMain.Location = new System.Drawing.Point(0, 0);
+            this.mnuMain.Name = "mnuMain";
+            this.mnuMain.Size = new System.Drawing.Size(674, 24);
+            this.mnuMain.TabIndex = 22;
+            this.mnuMain.Text = "menuStrip1";
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFileNew,
+            this.mnuFileOpen,
+            this.mnuFileSave,
+            this.mnuFileSaveAs,
+            this.toolStripMenuItem1,
+            this.mnuFileExit});
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(37, 20);
+            this.mnuFile.Text = "&File";
+            // 
+            // mnuFileNew
+            // 
+            this.mnuFileNew.Name = "mnuFileNew";
+            this.mnuFileNew.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.mnuFileNew.Size = new System.Drawing.Size(155, 22);
+            this.mnuFileNew.Text = "&New";
+            this.mnuFileNew.Click += new System.EventHandler(this.mnuFileNew_Click);
+            // 
+            // mnuFileOpen
+            // 
+            this.mnuFileOpen.Name = "mnuFileOpen";
+            this.mnuFileOpen.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.mnuFileOpen.Size = new System.Drawing.Size(155, 22);
+            this.mnuFileOpen.Text = "&Open...";
+            this.mnuFileOpen.Click += new System.EventHandler(this.mnuFileOpen_Click);
+            // 
+            // mnuFileSave
+            // 
+            this.mnuFileSave.Name = "mnuFileSave";
+            this.mnuFileSave.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.mnuFileSave.Size = new System.Drawing.Size(155, 22);
+            this.mnuFileSave.Text = "&Save";
+            this.mnuFileSave.Click += new System.EventHandler(this.mnuFileSave_Click);
+            // 
+            // mnuFileSaveAs
+            // 
+            this.mnuFileSaveAs.Name = "mnuFileSaveAs";
+            this.mnuFileSaveAs.Size = new System.Drawing.Size(155, 22);
+            this.mnuFileSaveAs.Text = "Save &As...";
+            this.mnuFileSaveAs.Click += new System.EventHandler(this.mnuFileSaveAs_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(152, 6);
+            // 
+            // mnuFileExit
+            // 
+            this.mnuFileExit.Name = "mnuFileExit";
+            this.mnuFileExit.Size = new System.Drawing.Size(155, 22);
+            this.mnuFileExit.Text = "E&xit";
+            this.mnuFileExit.Click += new System.EventHandler(this.mnuFileExit_Click);
+            // 
+            // dlgOpen
+            // 
+            this.dlgOpen.DefaultExt = "xml";
+            this.dlgOpen.FileName = "fog_creek_vacation";
+            this.dlgOpen.Filter = "XML Files (*.xml)|*.xml|All files (*.*)|*.*";
+            this.dlgOpen.Title = "Open vacation data...";
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(674, 428);
+            this.ClientSize = new System.Drawing.Size(674, 450);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.btnRemove);
             this.Controls.Add(this.btnAdd);
@@ -274,14 +365,19 @@
             this.Controls.Add(this.txtCurrBalance);
             this.Controls.Add(this.dtCurr);
             this.Controls.Add(this.lblCurr);
+            this.Controls.Add(this.mnuMain);
             this.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MinimumSize = new System.Drawing.Size(690, 409);
+            this.MainMenuStrip = this.mnuMain;
+            this.MinimumSize = new System.Drawing.Size(690, 431);
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Fogcation - Fog Creek Vacation Calculator";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
             this.Resize += new System.EventHandler(this.Main_Resize);
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.mnuMain.ResumeLayout(false);
+            this.mnuMain.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -312,6 +408,16 @@
         private System.Windows.Forms.ColumnHeader colTime;
         private System.Windows.Forms.Label lblVacation;
         private System.Windows.Forms.ColumnHeader colDelta;
+        private System.Windows.Forms.SaveFileDialog dlgSave;
+        private System.Windows.Forms.MenuStrip mnuMain;
+        private System.Windows.Forms.ToolStripMenuItem mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileOpen;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileSave;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileSaveAs;
+        private System.Windows.Forms.OpenFileDialog dlgOpen;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileNew;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileExit;
     }
 }
 
