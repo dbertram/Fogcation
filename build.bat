4f04540353c9c584b5084b3b051528aca9cdce1a
Dane Bertram <dbertram@users.noreply.github.com>
2011-01-13 13:18:14 -0500

    add build.bat

---
+++ build.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+ECHO C:\WINDOWS\Microsoft.NET\Framework\v3.5\msbuild.exe /nologo /verbosity:quiet /p:Configuration=Release
+CALL C:\WINDOWS\Microsoft.NET\Framework\v3.5\msbuild.exe /nologo /verbosity:quiet /p:Configuration=Release
